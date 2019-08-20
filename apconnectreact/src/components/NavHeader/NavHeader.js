d37bdff61e66f14fbc872029c04e30f5aa281f49
saiaakash <p.saiaakash518@gmail.com>
2019-08-20 23:43:17 +0530

    version 1.2

--- apconnectreact/src/components/NavHeader/NavHeader.js
+++ apconnectreact/src/components/NavHeader/NavHeader.js
@@ -8,7 +8,7 @@ render(){
 return(
  
 <Navbar bg="light" expand="lg" >
-  <Navbar.Brand href="#home">Send Files .tk<h6>Version 1.1</h6></Navbar.Brand>
+  <Navbar.Brand href="#home">Send Files .tk<h6>Version 1.2</h6></Navbar.Brand>
  
 </Navbar>
 
