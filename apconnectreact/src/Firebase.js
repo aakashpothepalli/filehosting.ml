f3b45bdff2c8c56192b2bd9a861700f41a142b50
Aakash Pothepalli <p.saiaakash518@gmail.com>
2021-01-04 22:07:01 +0530

    Update Firebase.js

--- apconnectreact/src/Firebase.js
+++ apconnectreact/src/Firebase.js
@@ -1,12 +1,6 @@
 import firebase from "firebase"
 var firebaseConfig = {
-    apiKey: "AIzaSyAAYUvCrzfkzs-A7-12sBR7nKQTIqUPPJo",
-    authDomain: "apconnect-a3812.firebaseapp.com",
-    databaseURL: "https://apconnect-a3812.firebaseio.com",
-    projectId: "apconnect-a3812",
-    storageBucket: "apconnect-a3812.appspot.com",
-    messagingSenderId: "929170999167",
-    appId: "1:929170999167:web:7ab1ae81cc1f17ba"
+  //get new api keys
   };
   // Initialize Firebase
   
