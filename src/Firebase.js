8972658595d186f9297cd1dc74dbc3c05f056f04
aakash9518 <p.saiaakash518@gmail.com>
2021-05-06 00:51:41 +0530

    restored firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,12 @@
 import firebase from "firebase"
 var firebaseConfig = {
-  //get new api keys
+  apiKey: "AIzaSyAAYUvCrzfkzs-A7-12sBR7nKQTIqUPPJo",
+  authDomain: "apconnect-a3812.firebaseapp.com",
+  databaseURL: "https://apconnect-a3812.firebaseio.com",
+  projectId: "apconnect-a3812",
+  storageBucket: "apconnect-a3812.appspot.com",
+  messagingSenderId: "929170999167",
+  appId: "1:929170999167:web:7ab1ae81cc1f17ba"
   };
   // Initialize Firebase
   
